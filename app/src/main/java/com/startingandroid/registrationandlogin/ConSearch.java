37dba93919b8576a44db2a7e33d9018929786d77
IndaH <swapibellad@gmail.com>
2016-10-11 01:30:49 +0530

    Upload

---
+++ app/src/main/java/com/startingandroid/registrationandlogin/ConSearch.java
@@ -0,0 +1,13 @@
+package com.startingandroid.registrationandlogin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ConSearch extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_con_search);
+    }
+}
